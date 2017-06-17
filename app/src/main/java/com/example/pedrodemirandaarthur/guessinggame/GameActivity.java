d3f1313976c8d705218d99ddb2499999dcc204e9
Pedro De Miranda Arthur <pedrohma95@gmail.com>
2017-06-17 13:29:34 -0700

    Implementing game logic

--- app/src/main/java/com/example/pedrodemirandaarthur/guessinggame/GameActivity.java
+++ app/src/main/java/com/example/pedrodemirandaarthur/guessinggame/GameActivity.java
@@ -3,7 +3,13 @@ package com.example.pedrodemirandaarthur.guessinggame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 /**
  * Created by pedrodemirandaarthur on 6/17/17.
@@ -11,27 +17,63 @@ import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-    TextView clue;
+    TextView clue, hideWord;
+    Button btnTry;
+    EditText inputLetter;
+    String clueWord, word, guessingLetter;
+    char[] arrayWord;
+    String[] arrayGuessedLetters;
 
-    String clueWord;
+    boolean isOk = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
+
         Intent i = getIntent();
+
         clue = (TextView) findViewById(R.id.clue);
+        hideWord = (TextView) findViewById(R.id.hideWord);
+        btnTry = (Button) findViewById(R.id.btnTry);
+        inputLetter = (EditText) findViewById(R.id.inputLetter);
 
         clueWord =  (String) i.getSerializableExtra("clueWord");
+        word =  (String) i.getSerializableExtra("word");
+
+        arrayWord = word.toCharArray();
+        Log.d("array", "array " + Arrays.toString(arrayWord));
 
         clue.setText(clueWord);
+        hideWord.setText(HideWord(word));
+
+        btnTry.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                guessingLetter = inputLetter.getText().toString();
+
+                if(guessingLetter.length() > 1){
+                    inputLetter.setError("Just one letter per time.");
+                    isOk = false;
+                }
+                if(guessingLetter.isEmpty()){
+                    inputLetter.setError("Can't be empty.");
+                    isOk = false;
+                }
+                if(isOk){
+
+                }
+            }
+        });
     }
 
     public String HideWord(String word) {
 
         String hideWord = "";
         for (int i = 0; i < word.length(); i++) {
-            hideWord = "_" + " ";
+            hideWord = hideWord + "_" + " ";
         }
         return hideWord;
     }
